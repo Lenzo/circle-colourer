0099bec0c026be9667d4fcd415fdd190929880a6
Lenzo <lenbunce@Lens-MacBook-Pro.local>
2016-03-31 13:36:23 -0400

    shape picker not working

--- js/main.js
+++ js/main.js
@@ -2,13 +2,25 @@ var $circle = $('.circle');
 var $form = $('form');
 var $color = $('#color');
 var $size = $('#size');
+var $newCircle = $('.new-circle');
+
+// var $shape-square = $('[value="square"]:checked');
+// var $shape-cirlce = $('#circle')
+
 
 $form.on('change', function() {
   $circle.css('background-color', $color.val());
 
   $circle.css('width', $size.val());
   $circle.css('height', $size.val());
-
-  $circle.css('')
 });
+
+$newCircle.on('click', function (){
+  
+}); 
+
+
+// if ($shape-square) {
+//   $cirlce.css('border-radius', 0);
+// };
 
